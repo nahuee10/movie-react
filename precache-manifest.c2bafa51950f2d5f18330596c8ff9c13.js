dbd3971ef9f057f4cc99bda3fa545ec1346c9a1d
Nahuel <nahuee.10.96@gmail.com>
2020-04-24 23:13:33 -0300

    Updates

---
+++ precache-manifest.c2bafa51950f2d5f18330596c8ff9c13.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f16ceeac70ca7830dc3445aa3aed09",
+    "url": "/movie-react/index.html"
+  },
+  {
+    "revision": "695ffbe4cc69a4d0f282",
+    "url": "/movie-react/static/css/2.a72c35d3.chunk.css"
+  },
+  {
+    "revision": "a218477954e9169ae449",
+    "url": "/movie-react/static/css/main.0d502662.chunk.css"
+  },
+  {
+    "revision": "695ffbe4cc69a4d0f282",
+    "url": "/movie-react/static/js/2.cccc677e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/movie-react/static/js/2.cccc677e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a218477954e9169ae449",
+    "url": "/movie-react/static/js/main.9cf44118.chunk.js"
+  },
+  {
+    "revision": "a69de3c18b1585a15c83",
+    "url": "/movie-react/static/js/runtime-main.b81f0d38.js"
+  },
+  {
+    "revision": "cb90c5bda3dbe4e16f7607c0c5213f3a",
+    "url": "/movie-react/static/media/original.cb90c5bd.svg"
+  }
+]);
